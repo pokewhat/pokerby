c658ba8e865a97e1759325e87a7c4663e9b49dad
Buggem <66670928+Buggem@users.noreply.github.com>
2022-07-05 16:12:22 +1000

    this one isn't my mistake, its the stupid programming languages
    why no await outside of a async function?

--- search.js
+++ search.js
@@ -1,8 +1,12 @@
 var toappendto = document.getElementsByClassName("contentbck")[0];
 var toreadhtml = new FileReader();
-var theurl = "https://pokewhat.github.io/pokerby/search.html"
+var theurl = "https://pokewhat.github.io/pokerby/search.html";
+var getBlob = async function(urlparam) {
+  let theblob = await fetch(urlparam).then(r => r.blob());
+  return theblob;
+};
 toreadhtml.onload = function(event) {
   toappendto.insertAdjacentHTML("beforeend", event.target.result);
 };
-let theblob = await fetch(theurl).then(r => r.blob());
+let theblob = async getBlob(theurl);
 toreadhtml.readAsText(theblob);
