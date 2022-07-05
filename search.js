4dd5f9346068dc8c40f08fa36a60e2b0d1820a39
Buggem <66670928+Buggem@users.noreply.github.com>
2022-07-05 16:45:06 +1000

    Update search.js

--- search.js
+++ search.js
@@ -2,18 +2,19 @@ var toappendto = document.getElementsByClassName("contentbck")[0];
 var toreadhtml = new FileReader();
 var theurl = "https://pokewhat.github.io/pokerby/search.html";
 var getBlob = async function(urlparam) {
+  var toReturn = null;
   let theblob = await fetch(urlparam).then((value) => {
-    return value;
+    toReturn = value;
   }, function() {
-    return null;
+    toReturn = null;
   });
 };
 toreadhtml.onload = function(event) {
   toappendto.insertAdjacentHTML("beforeend", event.target.result);
 };
 let theblob = getBlob(theurl);
+console.log(theblob);
 if(theblob !== null && typeof(theblob) == 'Blob') {
-  console.log(theblob);
   toreadhtml.readAsText(theblob);
 } else {
   console.log("Blob gather failed...");
