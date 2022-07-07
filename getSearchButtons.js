cac68b3048bc20a1e4ccd7919d701cbb8a538e0b
Buggem <66670928+Buggem@users.noreply.github.com>
2022-07-07 16:03:10 +1000

    Update getSearchButtons.js
    help the errororooroeororoeoroo

--- getSearchButtons.js
+++ getSearchButtons.js
@@ -1,8 +1,14 @@
 reponame = "pokerby";
+var loadItAll = function() {
+  if(!(!(document.getElementById("submitpoke") == undefined) || !(document.getElementById("poke") == undefined) || !(document.querySelectorAll("base")[0] == undefined))) {
+    this();
+    return;
+  }
   var getPokeUrl = function(poke, base) {
-  return (base + reponame + "/pokemon/" + poke);
-}
-document.getElementById("submitpoke").addEventListener("click", function() {
-  var theBase = document.querySelectorAll("base")[0].getAttribute("href");
-  window.location.href = (getPokeUrl(document.getElementById("poke").value, theBase));
-});
+    return (base + reponame + "/pokemon/" + poke);
+  };
+  document.getElementById("submitpoke").addEventListener("click", function() {
+    var theBase = document.querySelectorAll("base")[0].getAttribute("href");
+    window.location.href = (getPokeUrl(document.getElementById("poke").value, theBase));
+  });
+};
